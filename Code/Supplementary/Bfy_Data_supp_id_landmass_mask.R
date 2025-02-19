97ff6db2b94b4b1212a5917770d4a69c1c470436
Eugene <156638558+eugeneyau@users.noreply.github.com>
2025-02-19 14:17:23 +0800

    Add files via upload

---
+++ Code/Supplementary/Bfy_Data_supp_id_landmass_mask.R
@@ -0,0 +1,280 @@
+### This script was annotated with the help of generative AI with internet access ###
+
+########################################################
+##################      Read  Me      ##################
+
+# 1. Generate cleaned_occurrence.shp (vector) with this script
+
+# 2. Input vector generated into QGIS
+
+# 3. Use point sampling tool (plugin) 
+#    Get values from null_mask 
+
+# 4. Export:
+#    occ rec vector -> speciesname_lohman.csv
+#    point sampling tool output -> spzone_lohman.csv
+
+########################################################
+########################################################
+
+### 1. Generate cleaned_occurrence.shp
+
+# Load necessary libraries
+library(maptools)
+library(BBmisc)
+library(dplyr)
+library(terra)
+
+# Set working directory
+wd <- "/lustre1/g/sbs_bonebrake/Eugene/SDMin"
+setwd(wd)
+
+
+# Define truncation limits for the study area
+Xmini <- 69 #left x-axis, originally 64
+Xmaxi <- 161.6 #right x-axis, originally 166
+Ymini <- -10 #lower y-axis, originally -13
+Ymaxi <- 36 #upper y-axis, originally 37
+
+#Import species data
+data <- read.csv("Occurrence Records of Tropical Asian Butterflies - 1970-2024 (19Feb2025EJ).csv")
+
+# Standardize column names for latitude and longitude
+colnames(data) <- gsub("decimalLat","decimalLatitude",colnames(data))
+colnames(data) <- gsub("decimalLon","decimalLongitude",colnames(data))
+
+# Convert latitude and longitude to numeric
+data$decimalLatitude <- as.numeric(data$decimalLatitude)
+data$decimalLongitude <- as.numeric(data$decimalLongitude)
+
+# Prepare data by filtering based on defined geographical limits
+data <- subset(data,decimalLatitude>=Ymini)
+data <- subset(data,decimalLatitude<=Ymaxi)
+data <- subset(data,decimalLongitude>=Xmini)
+data <- subset(data,decimalLongitude<=Xmaxi)
+
+# Convert year to numeric and filter out records before 1970
+data$year <- as.numeric(data$year)
+data <- data[! c(1:nrow(data)) %in% which(data$year<1970),]
+
+# Correct species names (R does not like "-"s)
+data[data$lohman_final_genus_species == "Polygonia c-album","lohman_final_genus_species"] <- "Polygonia c album"
+data[data$lohman_final_genus_species == "Polygonia c-aureum","lohman_final_genus_species"] <- "Polygonia c aureum"
+
+# Do the analysis only for species with >= 10 records
+Count <- data %>% count(lohman_final_genus_species) #Count number of records, Feeds data into count(), saves result in count
+Count <- subset(Count, n > 9) #Threshold was 25, changed to 10 for more species
+
+# Clean the data to include only species with sufficient records
+Cleaned.data <- data[which(data$lohman_final_genus_species %in% Count$lohman_final_genus_species),]
+
+# Create a sorted list of unique species names
+Species.list <- sort(unique(Cleaned.data$lohman_final_genus_species)) #Alphabetically sort species name
+
+# Further prepare data for analysis
+SDM_data <- Cleaned.data[!is.na(Cleaned.data$decimalLatitude),] #Remove negative/NA data
+myRespXY <- SDM_data[,c("lohman_final_genus_species","decimalLatitude","decimalLongitude")] #Draws out latitude and longitude for each records
+Projected.co <- terra::vect(myRespXY, geom=c("decimalLongitude","decimalLatitude"),crs="epsg:4326") #Converts to point file and give CRS
+
+writeVector(Projected.co, "/lustre1/g/sbs_bonebrake/Eugene/SDMout/cleanedoccLohman/cleaned_occurrence.shp", filetype="ESRI Shapefile", layer=NULL, insert=FALSE,
+            overwrite=FALSE, options="ENCODING=UTF-8")
+
+
+### [QGIS] ###
+### 2. Input vector generated into QGIS
+### 3. Use point sampling tool (plugin), get values from null_mask
+### 4. Generate speciesname_lohman.csv and spzone_lohman.csv
+### [QGIS] ###
+
+
+### 5. Get speciesname_lohman.csv and spzone_lohman.csv
+
+# Set working directory
+wd <- "/lustre1/g/sbs_bonebrake/Eugene/SDMin/landmaskdata_2025"
+setwd(wd)
+
+# Read species name and zone data from CSV files
+recname <- read.csv("speciesname_lohman.csv")
+reczone <- read.csv("spzone_lohman.csv")
+
+# Initialize a data frame to store occurrence zones
+columns <- c("Species","Zone") 
+occzones <- data.frame(matrix(nrow = 0, ncol = length(columns))) 
+colnames(occzones) <- columns
+
+# Combine species names and zones into a single data frame
+occzones <- rbind(occzones, data.frame("Species"=recname[,1], 
+                                       "Zone"=reczone[,1]))
+
+# Initialize a data frame for recording species presence in different zones 
+columns <- c("Species","borneo","borneoperc","eastasia","eastasiaperc","india","indiaperc",
+             "indonesia","indonesiaperc","japan","japanperc","malaysia","malaysiaperc",
+             "newguinea","newguineaperc","oceania","oceaniaperc","philippines","philippinesperc",
+             "seasia","seasiaperc","taiwan","taiwanperc") 
+spzones <- data.frame(matrix(nrow = 0, ncol = length(columns))) 
+colnames(spzones) <- columns
+
+# Loop through each species to record occurrence in different zones
+for(m in 1:length(Species.list)) {
+  sp_data <- subset(occzones, Species == Species.list[m]) # Get data for the current species
+  
+  total <- nrow(sp_data) # Total occurrences for the species
+  
+  # Calculate occurrences and percentages for each zone
+  borneo <- nrow(subset(sp_data,Zone == "borneo"))
+  borneoperc <- borneo/total
+  
+  eastasia <- nrow(subset(sp_data,Zone == "eastasia"))
+  eastasiaperc <- eastasia/total
+  
+  india <- nrow(subset(sp_data,Zone == "india"))
+  indiaperc <- india/total
+  
+  indonesia <- nrow(subset(sp_data,Zone == "indonesia"))
+  indonesiaperc <- indonesia/total
+  
+  japan <- nrow(subset(sp_data,Zone == "japan"))
+  japanperc <- japan/total
+  
+  malaysia <- nrow(subset(sp_data,Zone == "malaysia"))
+  malaysiaperc <- malaysia/total
+  
+  oceania <- nrow(subset(sp_data,Zone == "oceania"))
+  oceaniaperc <- oceania/total
+  
+  newguinea <- nrow(subset(sp_data,Zone == "newguinea"))
+  newguineaperc <- newguinea/total
+  
+  philippines <- nrow(subset(sp_data,Zone == "philippines"))
+  philippinesperc <- philippines/total
+  
+  seasia <- nrow(subset(sp_data,Zone == "seasia"))
+  seasiaperc <- seasia/total
+  
+  taiwan <- nrow(subset(sp_data,Zone == "taiwan"))
+  taiwanperc <- taiwan/total
+  
+  # Record data to data frame
+  spzones <- rbind(spzones, data.frame("Species"=Species.list[[m]], 
+                                       "borneo"=borneo,
+                                       "borneoperc"=borneoperc,
+                                       "eastasia"=eastasia,
+                                       "eastasiaperc"=eastasiaperc,
+                                       "india"=india,
+                                       "indiaperc"=indiaperc,
+                                       "indonesia"=indonesia,
+                                       "indonesiaperc"=indonesiaperc,
+                                       "japan"=japan,
+                                       "japanperc"=japanperc,
+                                       "malaysia"=malaysia,
+                                       "malaysiaperc"=malaysiaperc,
+                                       "newguinea"=newguinea,
+                                       "newguineaperc"=newguineaperc,
+                                       "oceania"=oceania,
+                                       "oceaniaperc"=oceaniaperc,
+                                       "philippines"=philippines,
+                                       "philippinesperc"=philippinesperc,
+                                       "seasia"=seasia,
+                                       "seasiaperc"=seasiaperc,
+                                       "taiwan"=taiwan,
+                                       "taiwanperc"=taiwanperc))
+}
+
+# Initialize a boolean data frame for zones each single species has been observed in
+columns <- c("Species","borneo","eastasia","india","indonesia","japan","malaysia",
+             "newguinea","oceania","philippines","seasia","taiwan") 
+boolspzone <- data.frame(matrix(nrow = 0, ncol = length(columns))) 
+colnames(boolspzone) <- columns
+
+# Loop through each row to determine presence in each zone
+for(i in 1:nrow(data)){
+  
+  Species <- data$Species[i]
+  
+  # Species considered present and established in zones containing >= 1% of all records of that species
+  if(data$borneoperc[i]>=0.01){
+    borneo="TRUE"
+  }else{
+    borneo="FALSE"
+  }
+  
+  if(data$eastasiaperc[i]>=0.01){
+    eastasia="TRUE"
+  }else{
+    eastasia="FALSE"
+  }
+  
+  if(data$indiaperc[i]>=0.01){
+    india="TRUE"
+  }else{
+    india="FALSE"
+  }
+  
+  if(data$indonesiaperc[i]>=0.01){
+    indonesia="TRUE"
+  }else{
+    indonesia="FALSE"
+  }
+  
+  if(data$japanperc[i]>=0.01){
+    japan="TRUE"
+  }else{
+    japan="FALSE"
+  }
+  
+  if(data$malaysiaperc[i]>=0.01){
+    malaysia="TRUE"
+  }else{
+    malaysia="FALSE"
+  }
+  
+  if(data$newguineaperc[i]>=0.01){
+    newguinea="TRUE"
+  }else{
+    newguinea="FALSE"
+  }
+  
+  if(data$oceaniaperc[i]>=0.01){
+    oceania="TRUE"
+  }else{
+    oceania="FALSE"
+  }
+  
+  if(data$philippinesperc[i]>=0.01){
+    philippines="TRUE"
+  }else{
+    philippines="FALSE"
+  }
+  
+  if(data$seasiaperc[i]>=0.01){
+    seasia="TRUE"
+  }else{
+    seasia="FALSE"
+  }
+  
+  if(data$taiwanperc[i]>=0.01){
+    taiwan="TRUE"
+  }else{
+    taiwan="FALSE"
+  }
+  
+  # Record species presences in data frame
+  boolspzone <- rbind(boolspzone, data.frame("Species"=Species, 
+                                             "borneo"=borneo,
+                                             "eastasia"=eastasia,
+                                             "india"=india,
+                                             "indonesia"=indonesia,
+                                             "japan"=japan,
+                                             "malaysia"=malaysia,
+                                             "newguinea"=newguinea,
+                                             "oceania"=oceania,
+                                             "philippines"=philippines,
+                                             "seasia"=seasia,
+                                             "taiwan"=taiwan))
+  
+}
+
+# Save result to a CSV file
+write.csv(boolspzone, "Species_zones_lohman_bool.csv")
+
+
---
+++ Code/Supplementary/Bfy_Data_supp_unmodelled species_point_richness.R
@@ -0,0 +1,134 @@
+### This script was annotated with the help of generative AI with internet access ###
+
+# Load necessary libraries
+library(maptools)
+library(BBmisc)
+library(dplyr)
+library(terra)
+
+# Define extent of the study area
+Xmini <- 69       # Left x-axis limit
+Xmaxi <- 161.6    # Right x-axis limit
+Ymini <- -10      # Lower y-axis limit
+Ymaxi <- 36       # Upper y-axis limit
+
+# Set parameters for output files
+resolution <- 10000  # Resolution for raster output
+buffer_res <- 30000  # Buffer size for spatial data points
+outputname <- paste0("point_richness_unmodelled",buffer_res)
+existingoutput_dirname <- "biomod_4.2.4_datapp_2025" # Directory name of existing outputs
+
+# Create a directory for model output files
+model.files.dir <- paste0("results_", outputname)
+dir.create(paste0("/lustre1/g/sbs_bonebrake/Eugene/SDMout/",model.files.dir)) 
+
+# List modelled species
+splist <- list.files(paste0("/lustre1/g/sbs_bonebrake/Eugene/SDMout/clipped_maps_",existingoutput_dirname))
+
+# Set working directory for data processing
+wd <- "/lustre1/g/sbs_bonebrake/Eugene/SDMin"
+setwd(wd)
+
+# Import our occurrence dataset
+data <- read.csv("Occurrence Records of Tropical Asian Butterflies - 1970-2024 (19Feb2025EJ).csv")
+
+# Format data
+colnames(data) <- gsub("decimalLat","decimalLatitude",colnames(data))
+colnames(data) <- gsub("decimalLon","decimalLongitude",colnames(data))
+
+data$decimalLatitude <- as.numeric(data$decimalLatitude)
+data$decimalLongitude <- as.numeric(data$decimalLongitude)
+
+# Truncate data to study area
+data <- subset(data,decimalLatitude>=Ymini)
+data <- subset(data,decimalLatitude<=Ymaxi)
+data <- subset(data,decimalLongitude>=Xmini)
+data <- subset(data,decimalLongitude<=Xmaxi)
+
+# Filter out records with years before 1970
+data$year <- as.numeric(data$year)
+data <- data[! c(1:nrow(data)) %in% which(data$year<1970),]
+
+# Format species names
+data[data$lohman_final_genus_species == "Polygonia c-album","lohman_final_genus_species"] <- "Polygonia c album"
+data[data$lohman_final_genus_species == "Polygonia c-aureum","lohman_final_genus_species"] <- "Polygonia c aureum"
+
+# List all species names
+Species.list <- sort(unique(data$lohman_final_genus_species))
+
+# Format splist (modelled species)
+splist <- gsub("_ensemble_TSSbin.tif", "", splist)
+splist <- gsub("\\.", " ", splist)
+
+# Identify unmodelled species, record as Species.list
+bool <- Species.list %in% splist
+Pending.species.list <- Species.list[!bool]
+Species.list <- sort(Pending.species.list)
+
+# Load and prepare the world shapefile for future use
+Global1 <- terra::vect("world.shp")
+Global <- terra::crop(Global1,ext(Xmini,Xmaxi,Ymini,Ymaxi))
+Global <- terra::project(Global,"epsg:6933") 
+r.rast <- terra::rast(Global, resolution=c(resolution,resolution))
+r <- terra::rasterize(Global, r.rast)
+
+# Set the working directory for saving output files
+save_wd <- paste("/lustre1/g/sbs_bonebrake/Eugene/SDMout/",model.files.dir, sep="")
+setwd(save_wd) 
+
+# Function to create raster point buffer layer of occurrence points for each unmodelled species
+MyBiomodSF <- function(m) {
+  
+  # Extract data for the current species
+  SDM_data <- subset(data,lohman_final_genus_species == Species.list[m]) 
+  SDM_data <- SDM_data[!is.na(SDM_data$decimalLatitude),] #Remove negative/NA data
+  myRespXY <- SDM_data[,c("decimalLongitude","decimalLatitude")] #Draws out latitude and longitude for each records
+  
+  # Convert to spatial points with defined CRS
+  Projected.co <- terra::vect(myRespXY, geom=c("decimalLongitude","decimalLatitude"), crs="epsg:4326")
+  T.Projected.co <- terra::project(Projected.co,"epsg:6933")
+  T.Projected.co <- buffer(T.Projected.co, width=buffer_res) #Create a buffer around points
+  
+  # Rasterize the buffered points onto the base raster
+  T.Projected.co.raster <- terra::rasterize(T.Projected.co, r, vals=1)
+  T.Projected.co.raster[T.Projected.co.raster>=1] <- 1 #Ensure binary raster map output 
+  r[r>0] <- 0 #Reset raster values
+  
+  # Mask the raster to only include areas with knwon species occurrences (accessible regions)
+  map.raster <- mask(r, T.Projected.co.raster, inverse=FALSE, maskvalues=1, updatevalue=1)
+  
+  # Save the resulting raster as output
+  writeRaster(map.raster, paste0("/lustre1/g/sbs_bonebrake/Eugene/SDMout/",model.files.dir,"/",gsub(" |_",".",Species.list[m]),".tif"), overwrite=TRUE)
+  
+}
+
+# Use parallel processing to generate point density raster for all unmodelled species
+library(parallel)
+m <- 1:length(Species.list)
+mclapply(m, MyBiomodSF, mc.cores=8) #Assuming 8 cores available on your computer
+
+# List, extract, stack, and plot output (point density rasters)
+maps.list.out <- list.files(paste0("/lustre1/g/sbs_bonebrake/Eugene/SDMout/",model.files.dir,"/"))
+clippedmaps <- paste0("/lustre1/g/sbs_bonebrake/Eugene/SDMout/",model.files.dir,"/",maps.list.out)
+
+my_stack <- terra::rast(clippedmaps)  #Create a raster stack from output files
+alpha_point <- terra::app(my_stack, fun=sum, cores=8) #Calculate point richness
+plot(alpha_point) #Plot the point richness
+
+# Save point richness raster
+writeRaster(alpha_point, paste0("/lustre1/g/sbs_bonebrake/Eugene/Others_out/",outputname,"_alpha_point_density.tif"), overwrite=FALSE)
+
+# List, extract, and stack our SDM maps
+splist <- list.files(paste0("/lustre1/g/sbs_bonebrake/Eugene/SDMout/clipped_maps_",existingoutput_dirname))
+clippedmaps <- paste0("/lustre1/g/sbs_bonebrake/Eugene/SDMout/clipped_maps_",existingoutput_dirname,"/",splist)
+clipped_stack <- terra::rast(clippedmaps)
+clipped_stack_mean <- clipped_stack["EMmean"] #Use results of mean ensemble algorithm
+SDMoutput <- terra::app(clipped_stack_mean, fun=sum, cores=8)  #Get alpha diversity of modelled species
+
+# Stack, calculate and plot overall diversity from point richness and SDM output 
+overall_diversity <- c(SDMoutput, alpha_point) #Combine point richness output and SDM alpha diversity
+overall_diversity <- terra::app(overall_diversity, fun=sum, cores=8) #Sum overall diversity
+plot(overall_diversity) #Plot overall diversity
+
+# Save overall diversity raster
+writeRaster(overall_diversity, paste0("/lustre1/g/sbs_bonebrake/Eugene/Others_out/",outputname,"_overall_diversity.tif"), overwrite=FALSE)
